13a58a0243ac7cafc6d0f83db0f63ed3726294cc
Nush <steven370832@gmail.com>
2024-05-22 02:48:40 +0800

    init users 1

--- src/Users/admin.py
+++ src/Users/admin.py
@@ -1,1 +1,22 @@
 # Register your models here.
+from django.contrib import admin
+
+from Users.models.datas import Leftover, Waste
+from Users.models.user_info import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["users_id", "title", "email"]
+
+
+class WasteAdmin(admin.ModelAdmin):
+    list_display = ["id", "item", "provider", "sent_to"]
+
+
+class LeftoverAdmin(admin.ModelAdmin):
+    list_display = ["id", "item", "provider", "sent_to"]
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Waste, WasteAdmin)
+admin.site.register(Leftover, LeftoverAdmin)
