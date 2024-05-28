83d75f505072ce8cbb9291167d8642c872ab12cb
Nush <steven370832@gmail.com>
2024-05-28 17:17:03 +0800

    fix bugs

--- src/Board/admin.py
+++ src/Board/admin.py
@@ -1,19 +1,35 @@
-# Register your models here.
-
 from django.contrib import admin
 
+from .forms import BoardForm
 from .models import Board
 
 
 class BoardAdmin(admin.ModelAdmin):
+    form = BoardForm  # 指定使用自定義表單
     list_display = [
         "board_id",
-        "user_id",
+        "user",
+        "topic",
+        "date",
         "leftover_item",
         "waste_item",
         "fb_id",
         "wo_id",
     ]
+    fields = (
+        "topic",
+        "content",
+        "type",
+        "leftover_item",
+        "waste_item",
+        "fb_id",
+        "wo_id",
+    )
+    readonly_fields = ("date",)
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
 
 
 admin.site.register(Board, BoardAdmin)
