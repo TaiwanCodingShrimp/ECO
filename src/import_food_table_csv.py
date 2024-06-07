337e5ff6144956fa84daa788d6625795f89779f1
ShanWei-Ch <sammy.chang402@gmail.com>
2024-06-08 03:26:11 +0800

    Modify datas.py

---
+++ src/import_food_table_csv.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from Users.models.datas import Food_Table
+
+
+class create_food_Table:
+    
+    def run(self):
+        csv_file_path = "src/res/food_carbon_data.csv"
+        df = pd.read_csv(csv_file_path, nrows=50)
+
+        food_agencies = []
+
+        for index, row in df.iterrows():
+            food_agencies.append(
+                Food_Table(
+                    item = row["item"],
+                    carbon_footprint = row["carbon_footprint"],
+                )
+            )
+
+        Food_Table.objects.bulk_create(food_agencies, batch_size=500)
+
+        print("Data imported successfully!")
