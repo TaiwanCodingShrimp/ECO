2a6e82409ea285810470f75ba71f49c86c6e4985
Nush <113417764+nush-cc@users.noreply.github.com>
2024-06-11 20:00:06 +0800

    Organ (#40)
    * briefly add organizations frontend
    
    * briefly add organizations frontend
    
    * add food_bank into organizations frontend and add users basic info , button
    
    * donate function and organization frontend modify
    
    * change the type of Organizations orientation

--- src/Frontend/urls.py
+++ src/Frontend/urls.py
@@ -4,14 +4,16 @@ from .views import (
     board_detail_view,
     board_view,
     create_board_view,
+    donation_view,
+    food_bank_view,
     home_view,
     index_view,
     login_view,
     logout_view,
-    organizations_view,
     report_view,
     signup_view,
     users_view,
+    welfare_organization_view,
 )
 
 urlpatterns = [
@@ -23,7 +25,9 @@ urlpatterns = [
     path("home/board/", board_view, name="board"),
     path("home/board/createboard/", create_board_view, name="createboard"),
     path("home/board/<int:board_id>/", board_detail_view, name="board_detail"),
-    path("home/organizations/", organizations_view, name="organizations"),
-    path("home/report/", report_view, name="report"),
+    path("home/users/report/", report_view, name="report"),
+    path("home/users/WelfareOrganization/", welfare_organization_view, name="welfare"),
+    path("home/users/FoodBank", food_bank_view, name="foodbank"),
+    path("home/users/donation/", donation_view, name="donation"),
     path("home/users/", users_view, name="users"),
 ]
